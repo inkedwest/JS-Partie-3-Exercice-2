1ed423f7b596ac16637d5512af19a1f4e1bc3764
Glatigny <f.west@live.fr>
2019-01-28 15:56:02 +0100

    exo 2

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// On appelle la fonction pour afficher le message
+function show(){
+  document.getElementById('text').style.visibility="visible";
+}
+// On appelle la fonction pour masquer le message
+function hide(){
+  document.getElementById('text').style.visibility="hidden";
+}
